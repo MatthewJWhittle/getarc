708725566e6ca1cd961ce6e2358099ef6fec1142
MatthewJWhittle <matthewjwhittle@gmail.com>
2021-02-02 14:59:42 +0000

    generate token handling

--- R/parse_access_token.R
+++ R/parse_access_token.R
@@ -8,5 +8,8 @@
 parse_access_token <-
   function(my_token){
     if(is.null(my_token)){return(my_token)}
+    # This allows us to deal with access tokens that are generate by generate Tokens process
+    # Not by oauth2.0
+    if(is.character(my_token)){return(my_token)}
     my_token$credentials$access_token
   }
