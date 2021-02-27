7292c40edc5d0e3d66af8097a98fc8a42ce220bb
MatthewJWhittle <matthewjwhittle@gmail.com>
2021-02-27 18:05:43 +0000

    still fixing travis

--- tests/testthat/test-set_get_credentials.R
+++ tests/testthat/test-set_get_credentials.R
@@ -3,25 +3,16 @@ credentials <- list(client_id = "my client id",
                     client_secret = "my client secret",
                     app_name = "my app name")
 
-# Get the credentials that are to be overwritten
-# This allows the test to leave things as it found it
-old_credentials <- get_credentials()
-
 # Then set the new credentials
 set_credentials(
   client_id = credentials$client_id,
   client_secret = credentials$client_secret,
-  app_name = credentials$app_name
+  app_name = credentials$app_name,
+  "~/secrets/test-getarc-credentials.json"
 )
 
 # Get the newly set credentials
-new_credentials <- get_credentials()
-# Return the credentials to their previous value
-set_credentials(
-  client_id = old_credentials$client_id,
-  client_secret = old_credentials$client_secret,
-  app_name = old_credentials$app_name
-)
+new_credentials <- get_credentials(path = "~/secrets/test-getarc-credentials.json")
 
 testthat::test_that("Getting and setting credentials works", {
   expect_type(new_credentials, "list")
