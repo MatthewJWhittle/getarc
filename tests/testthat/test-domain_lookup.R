07b571cbddd2659ffa94935c7b225f9769371363
MatthewJWhittle <matthewjwhittle@gmail.com>
2021-04-02 08:40:07 +0100

    add more tests

--- tests/testthat/test-domain_lookup.R
+++ tests/testthat/test-domain_lookup.R
@@ -1,5 +1,5 @@
 endpoint <-
-  "https://services.arcgis.com//JJzESW51TqeY9uat/arcgis/rest/services/National_Parks_England/FeatureServer/0"
+  "https://services6.arcgis.com/k3kybwIccWQ0A7BB/arcgis/rest/services/Domain_Points/FeatureServer/0"
 
 layer_details <-
   get_layer_details(endpoint = endpoint)
@@ -8,4 +8,7 @@ domains <- domain_lookup(layer_details)
 
 test_that("column names will work downstream", {
   expect_equal(colnames(domains), c("field_name", "name", "code"))
+  expect_equal(domains$name, c("A", "B", "C", "D"))
+  expect_equal(domains$code, as.character(c(0:3)))
+  expect_equal(unique(domains$field_name), "test_domain")
 })
